2c9ddeee62ab945edab413db944fd380e426eb0b
panther706 <43943311+panther706@users.noreply.github.com>
2026-01-19 16:10:56 +0800

    Update imm-part1.sh

--- imm-part1.sh
+++ imm-part1.sh
@@ -7,4 +7,6 @@
 git clone -b master --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 git clone -b v5 --depth=1 https://github.com/sbwml/luci-app-mosdns package/mosdns
 git clone -b main --depth=1 https://github.com/sirpdboy/luci-app-adguardhome.git package/adguardhome
+git clone -b main --depth=1 https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
+git clone -b main --depth=1 https://github.com/sirpdboy/luci-app-wizard
 git clone -b main --depth=1 https://github.com/FUjr/QModem.git package/QModem
