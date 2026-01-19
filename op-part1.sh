e6b5b7844039ad6d5913f13228b3b3753413b3f3
panther706 <43943311+panther706@users.noreply.github.com>
2026-01-19 21:08:51 +0800

    Update op-part1.sh

--- op-part1.sh
+++ op-part1.sh
@@ -1,4 +1,4 @@
 git clone -b master --depth=1 https://github.com/vernesong/OpenClash.git package/OpenClash
-git clone -b master --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 git clone -b main --depth=1 https://github.com/sirpdboy/luci-app-adguardhome.git package/adguardhome
 git clone -b main --depth=1 https://github.com/FUjr/QModem.git package/QModem
